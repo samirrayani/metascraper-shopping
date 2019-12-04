eac6437a635dcb7dd625583e09dc901b76992244
Samir Rayani <samir.rayani@gmail.com>
2019-12-04 16:07:07 -0500

    formatPrice and update package

--- README.md
+++ README.md
@@ -3,8 +3,6 @@
 [![npm](https://img.shields.io/npm/v/metascraper-price.svg?style=flat-square)](https://www.npmjs.com/package/metascraper-price)
 [![Dependency Status](https://david-dm.org/samirrayani/metascraper-price.svg?style=flat-square)](https://david-dm.org/samirrayani/metascraper-price)
 
-
-
 > Get price property from HTML markup.
 
 ## Install
--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict'
 
+const formatPrice = (price) => {
+  return parseFloat(price.replace('$',''));
+}
+
 /**
  * A set of rules we want to declare under `metascraper-price` namespace.
  *
@@ -10,10 +14,10 @@ module.exports = () => {
       // They receive as parameter:
       // - `htmlDom`: the cheerio HTML instance.
       // - `url`: The input URL used for extact the content.
-      ({ htmlDom: $, url }) => $('[itemprop=price]').attr('content'),
-      ({ htmlDom: $, url }) => $('[itemprop=price]').html(),
-      ({ htmlDom: $, url }) => $('[property="og:price:amount"]').attr('content'),
-      ({ htmlDom: $, url }) => $('[property="product:price:amount"]').attr('content')
+      formatPrice(({ htmlDom: $, url }) => $('[itemprop=price]').attr('content')),
+      formatPrice(({ htmlDom: $, url }) => $('[property="og:price:amount"]').attr('content')),
+      formatPrice(({ htmlDom: $, url }) => $('[property="product:price:amount"]').attr('content')),
+      formatPrice(({ htmlDom: $, url }) => $('[itemprop=price]').html())
     ]
   }
   return rules
