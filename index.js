0316deb3e304a9e8553ba61aa6b84cd675ccb239
Samir Rayani <samir.rayani@gmail.com>
2019-12-04 16:19:59 -0500

    undo formatPrice

--- index.js
+++ index.js
@@ -1,9 +1,5 @@
 'use strict'
 
-const formatPrice = (price) => {
-  return parseFloat(price.replace('$',''));
-}
-
 /**
  * A set of rules we want to declare under `metascraper-price` namespace.
  *
@@ -14,10 +10,10 @@ module.exports = () => {
       // They receive as parameter:
       // - `htmlDom`: the cheerio HTML instance.
       // - `url`: The input URL used for extact the content.
-      ({ htmlDom: $, url }) => formatPrice($('[itemprop=price]').attr('content')),
-      ({ htmlDom: $, url }) => formatPrice($('[property="og:price:amount"]').attr('content')),
-      ({ htmlDom: $, url }) => formatPrice($('[property="product:price:amount"]').attr('content')),
-      ({ htmlDom: $, url }) => formatPrice($('[itemprop=price]').html())
+      ({ htmlDom: $, url }) => $('[itemprop=price]').attr('content'),
+      ({ htmlDom: $, url }) => $('[property="og:price:amount"]').attr('content'),
+      ({ htmlDom: $, url }) => $('[property="product:price:amount"]').attr('content'),
+      ({ htmlDom: $, url }) => $('[itemprop=price]').html()
     ]
   }
   return rules
