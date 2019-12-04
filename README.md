04d58343c9f1c783edbe2fb1118d0af6f9d08d51
Samir Rayani <samir.rayani@gmail.com>
2019-12-04 15:09:31 -0500

    first commit

---
+++ README.md
@@ -0,0 +1,9 @@
+# metascraper-price
+
+> Get price property from HTML markup.
+
+## Install
+
+```bash
+$ npm install metascraper-price --save
+```
---
+++ index.js
@@ -0,0 +1,20 @@
+'use strict'
+
+/**
+ * A set of rules we want to declare under `metascraper-price` namespace.
+ *
+**/
+module.exports = () => {
+  const rules = {
+    price: [
+      // They receive as parameter:
+      // - `htmlDom`: the cheerio HTML instance.
+      // - `url`: The input URL used for extact the content.
+      ({ htmlDom: $, url }) => $('[itemprop=price]').attr('content'),
+      ({ htmlDom: $, url }) => $('[itemprop=price]').html(),
+      ({ htmlDom: $, url }) => $('[property="og:price:amount"]').attr('content'),
+      ({ htmlDom: $, url }) => $('[property="product:price:amount"]').attr('content')
+    ]
+  }
+  return rules
+}
