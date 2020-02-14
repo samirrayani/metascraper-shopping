f7fc48fe8520f8ee041dff9d10825d3353ecb10a
Samir Rayani <samir.rayani@gmail.com>
2020-02-13 22:45:29 -0500

    update user-agent

--- test/live.test.js
+++ test/live.test.js
@@ -3,16 +3,15 @@
 const got = require('got');
 const metascraper = require('metascraper')([require('..')()])
 
-test.skip('live test', async () => {
-	const targetUrl = 'https://www.article.com/product/2525/beacon-copper-floor-lamp';
+test('live test', async () => {
+	const targetUrl = 'https://www.wayfair.com/Maykke--Dewey-59.06-x-31.5-Freestanding-Soaking-Bathtub-XDA1408001-L590-K~MYKK1019.html?refid=GX99081569362-MYKK1019&device=c&ptid=327664668846&network=g&targetid=pla-327664668846&channel=GooglePLA&ireid=54397045&fdid=1817';
 	const { body: html, url } = await got(targetUrl, {
 		'headers': {
-			'user-agent': 'googlebot'
+			'user-agent': 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_11_1) AppleWebKit/601.2.4 (KHTML, like Gecko) Version/9.0.1 Safari/601.2.4 facebookexternalhit/1.1 Facebot Twitterbot/1.0'
 		}
 	});
 
 	const metadata = await metascraper({ html, url });
-	console.log(metadata);
-	expect(metadata.currency).toBe('USD');
+	expect(metadata.hostname).toBe('wayfair.com');
 
 }, 10000);
