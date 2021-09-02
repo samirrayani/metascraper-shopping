0892edeb4bd1a0e93d8de0cc33fdeeb0c9213c55
Samir Rayani <samir.rayani@gmail.com>
2021-09-01 21:10:33 -0700

    get price + image from amazon

--- index.js
+++ index.js
@@ -1,65 +1,80 @@
-'use strict'
+"use strict";
 
-const { $jsonld } = require('@metascraper/helpers')
-const { toPriceFormat, getHostname } = require('./helpers');
+const { $jsonld } = require("@metascraper/helpers");
+const { toPriceFormat, getHostname } = require("./helpers");
 
 /**
  * A set of rules we want to declare under the `metascraper-shopping` namespace.
  *
-**/
+ **/
 module.exports = () => {
   const rules = {
     image: [
-      ({ htmlDom: $, url }) => $('div.ImageComponent img[data-codeception-id="ImageComponent"]').attr('src'), //wayfair.com
-      ({ htmlDom: $, url }) => $('img#comparison_image').attr('src'), //amazon.com      
+      ({ htmlDom: $, url }) =>
+        $('div.ImageComponent img[data-codeception-id="ImageComponent"]').attr(
+          "src"
+        ), //wayfair.com
+      ({ htmlDom: $, url }) => $("img#comparison_image").attr("data-src"), //amazon.com
     ],
     currency: [
-      ({ htmlDom: $, url }) => $('[property="og:price:currency"]').attr('content'),
-      ({ htmlDom: $, url }) => $jsonld('offers.0.priceCurrency')($,url),
-      ({ htmlDom: $, url }) => $jsonld('offers.priceCurrency')($,url),
-      ({ htmlDom: $, url }) => $('[data-asin-currency-code]').attr('data-asin-currency-code'), //amazon
-      ({ htmlDom: $, url }) => $('[property="product:price:currency"]').attr('content'),
-      ({ htmlDom: $, url }) => $('[itemprop=priceCurrency]').attr('content')
+      ({ htmlDom: $, url }) =>
+        $('[property="og:price:currency"]').attr("content"),
+      ({ htmlDom: $, url }) => $jsonld("offers.0.priceCurrency")($, url),
+      ({ htmlDom: $, url }) => $jsonld("offers.priceCurrency")($, url),
+      ({ htmlDom: $, url }) =>
+        $("[data-asin-currency-code]").attr("data-asin-currency-code"), //amazon
+      ({ htmlDom: $, url }) =>
+        $('[property="product:price:currency"]').attr("content"),
+      ({ htmlDom: $, url }) => $("[itemprop=priceCurrency]").attr("content"),
     ],
     condition: [
-      ({ htmlDom: $, url }) => $jsonld('itemCondition')($,url),
-      ({ htmlDom: $, url }) => $jsonld('offers.itemCondition')($,url),
-      ({ htmlDom: $, url }) => $jsonld('offers.0.itemCondition')($,url),
+      ({ htmlDom: $, url }) => $jsonld("itemCondition")($, url),
+      ({ htmlDom: $, url }) => $jsonld("offers.itemCondition")($, url),
+      ({ htmlDom: $, url }) => $jsonld("offers.0.itemCondition")($, url),
     ],
     sku: [
-      ({ htmlDom: $, url }) => $jsonld('sku')($,url),
-      ({ htmlDom: $, url }) => $jsonld('offers.sku')($,url),
-      ({ htmlDom: $, url }) => $jsonld('offers.0.sku')($,url),
-      ({ htmlDom: $, url }) => $('[itemprop=sku]').html(), 
+      ({ htmlDom: $, url }) => $jsonld("sku")($, url),
+      ({ htmlDom: $, url }) => $jsonld("offers.sku")($, url),
+      ({ htmlDom: $, url }) => $jsonld("offers.0.sku")($, url),
+      ({ htmlDom: $, url }) => $("[itemprop=sku]").html(),
     ],
-    mpn: [ //mpn=ManufacturProductNumber
-      ({ htmlDom: $, url }) => $jsonld('mpn')($,url),
-      ({ htmlDom: $, url }) => $jsonld('offers.mpn')($,url),
-      ({ htmlDom: $, url }) => $jsonld('offers.0.mpn')($,url),
+    mpn: [
+      //mpn=ManufacturProductNumber
+      ({ htmlDom: $, url }) => $jsonld("mpn")($, url),
+      ({ htmlDom: $, url }) => $jsonld("offers.mpn")($, url),
+      ({ htmlDom: $, url }) => $jsonld("offers.0.mpn")($, url),
     ],
     availability: [
-      ({ htmlDom: $, url }) => $('[property="og:availability"]').attr('content'),
-      ({ htmlDom: $, url }) => $jsonld('offers.availability')($,url),
-      ({ htmlDom: $, url }) => $jsonld('offers.0.availability')($,url),
+      ({ htmlDom: $, url }) =>
+        $('[property="og:availability"]').attr("content"),
+      ({ htmlDom: $, url }) => $jsonld("offers.availability")($, url),
+      ({ htmlDom: $, url }) => $jsonld("offers.0.availability")($, url),
     ],
     price: [
-      ({ htmlDom: $, url }) => toPriceFormat($('[property="og:price:amount"]').attr('content')),
-      ({ htmlDom: $, url }) => toPriceFormat($('[itemprop=price]').attr('content')), 
-      ({ htmlDom: $, url }) => toPriceFormat($('[property="product:price:amount"]').attr('content')),
-      ({ htmlDom: $, url }) => toPriceFormat($jsonld('price')($,url)),
-      ({ htmlDom: $, url }) => toPriceFormat($jsonld('offers.price')($,url)),
-      ({ htmlDom: $, url }) => toPriceFormat($jsonld('0.offers.price')($,url)),
-      ({ htmlDom: $, url }) => toPriceFormat($jsonld('offers.lowPrice')($,url)),
-      ({ htmlDom: $, url }) => toPriceFormat($jsonld('offers.highPrice')($,url)),
-      ({ htmlDom: $, url }) => toPriceFormat($('[data-asin-price]').attr('data-asin-price')), //amazon
-      ({ htmlDom: $, url }) => toPriceFormat($('[itemprop=price]').html())
+      ({ htmlDom: $, url }) =>
+        toPriceFormat($('[property="og:price:amount"]').attr("content")),
+      ({ htmlDom: $, url }) =>
+        toPriceFormat($("[itemprop=price]").attr("content")),
+      ({ htmlDom: $, url }) =>
+        toPriceFormat($('[property="product:price:amount"]').attr("content")),
+      ({ htmlDom: $, url }) => toPriceFormat($jsonld("price")($, url)),
+      ({ htmlDom: $, url }) => toPriceFormat($jsonld("offers.price")($, url)),
+      ({ htmlDom: $, url }) => toPriceFormat($jsonld("0.offers.price")($, url)),
+      ({ htmlDom: $, url }) =>
+        toPriceFormat($jsonld("offers.lowPrice")($, url)),
+      ({ htmlDom: $, url }) =>
+        toPriceFormat($jsonld("offers.highPrice")($, url)),
+      ({ htmlDom: $, url }) =>
+        toPriceFormat($("[data-asin-price]").attr("data-asin-price")), //amazon
+      ({ htmlDom: $, url }) => toPriceFormat($("[itemprop=price]").html()),
+      ({ htmlDom: $, url }) =>
+        toPriceFormat($("#attach-base-product-price").attr("value")),
     ],
-    asin: [ //unique amazon identifier
-      ({ htmlDom: $, url }) => $('[data-asin]').attr('data-asin'),
+    asin: [
+      //unique amazon identifier
+      ({ htmlDom: $, url }) => $("[data-asin]").attr("data-asin"),
     ],
-    hostname: [
-      ({ htmlDom: $, url }) => getHostname(url),
-    ]
-  }
-  return rules
-}
+    hostname: [({ htmlDom: $, url }) => getHostname(url)],
+  };
+  return rules;
+};
--- test/price.test.js
+++ test/price.test.js
@@ -1,10 +1,10 @@
-'use strict'
+"use strict";
 
-const { readFile } = require('fs').promises
-const { resolve } = require('path')
-const metascraper = require('metascraper')([require('..')()])
+const { readFile } = require("fs").promises;
+const { resolve } = require("path");
+const metascraper = require("metascraper")([require("..")()]);
 
-test('returns open graph value (og:price:amount)', async () => {
+test("returns open graph value (og:price:amount)", async () => {
   const html = `
     <!DOCTYPE html>
     <html lang="en">
@@ -19,13 +19,13 @@ test('returns open graph value (og:price:amount)', async () => {
     <body>
     </body>
     </html>
-    `
-  const url = 'https://www.strukbuilt.com/';
+    `;
+  const url = "https://www.strukbuilt.com/";
   const metadata = await metascraper({ html, url });
-  expect(metadata.price).toBe(100000000.00);
+  expect(metadata.price).toBe(100000000.0);
 });
 
-test('price with commas', async () => {
+test("price with commas", async () => {
   const html = `
     <!DOCTYPE html>
     <html lang="en">
@@ -39,57 +39,75 @@ test('price with commas', async () => {
     <body>
     </body>
     </html>
-    `
-  const url = 'https://www.strukbuilt.com/';
+    `;
+  const url = "https://www.strukbuilt.com/";
   const metadata = await metascraper({ html, url });
-  expect(metadata.price).toBe(2699.00);
+  expect(metadata.price).toBe(2699.0);
 });
 
-test('overstock.com ld+json', async () => {
-  const html = await readFile(resolve(__dirname, 'fixtures/overstock.html'))
-  const url = 'https://www.overstock.com/Home-Garden/Certified-International-Elegance-Canape-Plates-Set-of-6/29099468/product.html?refccid=LDNVKF2XQ7MKQP2LE3VXMUYALU&searchidx=0&guid=f4adad64-034d-46b2-92dc-e956d094d3ea&osp=true&option=51668490&kwds=&rfmt='
-  const metadata = await metascraper({ url, html })
+test("overstock.com ld+json", async () => {
+  const html = await readFile(resolve(__dirname, "fixtures/overstock.html"));
+  const url =
+    "https://www.overstock.com/Home-Garden/Certified-International-Elegance-Canape-Plates-Set-of-6/29099468/product.html?refccid=LDNVKF2XQ7MKQP2LE3VXMUYALU&searchidx=0&guid=f4adad64-034d-46b2-92dc-e956d094d3ea&osp=true&option=51668490&kwds=&rfmt=";
+  const metadata = await metascraper({ url, html });
   expect(metadata.price).toBe(31.49);
 });
 
-test('jossandmain.com ld+json', async () => {
-  const html = await readFile(resolve(__dirname, 'fixtures/jossandmain.html'))
-  const url = 'https://www.jossandmain.com/lighting/pdp/duprey-4-light-lantern-pendant-j001109161.html'
-  const metadata = await metascraper({ url, html })
+test("jossandmain.com ld+json", async () => {
+  const html = await readFile(resolve(__dirname, "fixtures/jossandmain.html"));
+  const url =
+    "https://www.jossandmain.com/lighting/pdp/duprey-4-light-lantern-pendant-j001109161.html";
+  const metadata = await metascraper({ url, html });
   expect(metadata.price).toBe(98.99);
 });
 
-test('costco.com product:price:amount', async () => {
-  const html = await readFile(resolve(__dirname, 'fixtures/costco.html'))
-  const url = 'https://www.costco.com/afa-stainless-33-inch-sink-and-semi-pro-faucet-combo.product.100432602.html'
-  const metadata = await metascraper({ url, html })
+test("costco.com product:price:amount", async () => {
+  const html = await readFile(resolve(__dirname, "fixtures/costco.html"));
+  const url =
+    "https://www.costco.com/afa-stainless-33-inch-sink-and-semi-pro-faucet-combo.product.100432602.html";
+  const metadata = await metascraper({ url, html });
   expect(metadata.price).toBe(329.99);
 });
 
-test('potterybarn.com no longer available', async () => {
-  const html = await readFile(resolve(__dirname, 'fixtures/potterybarn.html'))
-  const url = 'https://www.potterybarn.com/products/tully-brass-sconce/'
-  const metadata = await metascraper({ url, html })
+test("potterybarn.com no longer available", async () => {
+  const html = await readFile(resolve(__dirname, "fixtures/potterybarn.html"));
+  const url = "https://www.potterybarn.com/products/tully-brass-sconce/";
+  const metadata = await metascraper({ url, html });
   expect(metadata.price).toBeFalsy();
 });
 
-test('rejuvenation.com item on sale', async () => {
-  const html = await readFile(resolve(__dirname, 'fixtures/rejuvenation.html'))
-  const url = 'https://www.rejuvenation.com/catalog/collections/keystick-wall-sconce/products/5c50a4d2ccf2f73ea295b9e1'
-  const metadata = await metascraper({ url, html })
-  expect(metadata.price).toBe(120.00);
+test("rejuvenation.com item on sale", async () => {
+  const html = await readFile(resolve(__dirname, "fixtures/rejuvenation.html"));
+  const url =
+    "https://www.rejuvenation.com/catalog/collections/keystick-wall-sconce/products/5c50a4d2ccf2f73ea295b9e1";
+  const metadata = await metascraper({ url, html });
+  expect(metadata.price).toBe(120.0);
 });
 
-test('amazon.com asin price', async () => {
-  const html = await readFile(resolve(__dirname, 'fixtures/amazon.html'))
-  const url = 'https://www.amazon.com/Lighting-Fixture-Plaster-Conservatory-Dimmable/dp/B07G2L3JMM'
-  const metadata = await metascraper({ url, html })
+test("amazon.com asin price", async () => {
+  const html = await readFile(resolve(__dirname, "fixtures/amazon.html"));
+  const url =
+    "https://www.amazon.com/Lighting-Fixture-Plaster-Conservatory-Dimmable/dp/B07G2L3JMM";
+  const metadata = await metascraper({ url, html });
   expect(metadata.price).toBe(29.99);
 });
 
-test('globalindustrial.com NaN', async () => {
-  const html = await readFile(resolve(__dirname, 'fixtures/globalindustrial.html'))
-  const url = 'https://www.globalindustrial.com/p/work-benches/components/tops/60-x-24-maple-square-edge-bench-top'
-  const metadata = await metascraper({ url, html })
+test("amazon.com #attach-base-product-price price", async () => {
+  const html = await readFile(
+    resolve(__dirname, "fixtures/amazon-gorilla.html")
+  );
+  const url =
+    "https://www.amazon.com/Gorilla-6012110-Black-2-Pack-Piece/dp/B00N150YS4/ref=sr_1_3?dchild=1&keywords=gorilla+tape&qid=1630396817&sr=8-3";
+  const metadata = await metascraper({ url, html });
+  expect(metadata.price).toBe(12.48);
+});
+
+test("globalindustrial.com NaN", async () => {
+  const html = await readFile(
+    resolve(__dirname, "fixtures/globalindustrial.html")
+  );
+  const url =
+    "https://www.globalindustrial.com/p/work-benches/components/tops/60-x-24-maple-square-edge-bench-top";
+  const metadata = await metascraper({ url, html });
   expect(metadata.price).toBeFalsy();
 });
